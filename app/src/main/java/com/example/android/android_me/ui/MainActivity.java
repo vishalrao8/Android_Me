77e0c8b1fea5a580da619a102810eae5bc75ed71
Vishal Rao <vishal.rao103@gmail.com>
2018-08-25 02:32:36 +0530

    DONE

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -18,12 +18,13 @@ package com.example.android.android_me.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
 // This activity is responsible for displaying the master list of all images
-// TODO (4) Implement the MasterListFragment callback, OnImageClickListener
-public class MainActivity extends AppCompatActivity {
+// DONE (4) Implement the MasterListFragment callback, OnImageClickListener
+public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
 
 
     @Override
@@ -33,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // TODO (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked
+    @Override
+    public void onClick(int position) {
+        Toast.makeText(this, "Clicked position: "+ position, Toast.LENGTH_SHORT).show();
+    }
+
+    // DONE (5) Define the behavior for onImageSelected; create a Toast that displays the position clicked
 
 }
--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -16,11 +16,13 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.android.android_me.R;
@@ -31,12 +33,29 @@ import com.example.android.android_me.data.AndroidImageAssets;
 // The list appears as a grid of images
 public class MasterListFragment extends Fragment {
 
-    // TODO (1) Define a new interface OnImageClickListener that triggers a callback in the host activity
+    // DONE (1) Define a new interface OnImageClickListener that triggers a callback in the host activity
         // The callback is a method named onImageSelected(int position) that contains information about
         // which position on the grid of images a user has clicked
+    private OnImageClickListener onImageClickListener;
 
-    // TODO (2) Override onAttach to make sure that the container activity has implemented the callback
+    interface OnImageClickListener {
+        void onClick(int position);
+    }
 
+    // DONE (2) Override onAttach to make sure that the container activity has implemented the callback
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            onImageClickListener = (OnImageClickListener) context;
+        } catch (Exception e) {
+            throw new ClassCastException(context.toString() + "must implement required interface");
+        }
+
+    }
 
     // Mandatory empty constructor
     public MasterListFragment() {
@@ -59,7 +78,13 @@ public class MasterListFragment extends Fragment {
         // Set the adapter on the GridView
         gridView.setAdapter(mAdapter);
 
-        // TODO (3) Set a click listener on the gridView and trigger the callback onImageSelected when an item is clicked
+        // DONE (3) Set a click listener on the gridView and trigger the callback onImageSelected when an item is clicked
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                onImageClickListener.onClick(position);
+            }
+        });
 
         // Return the root view
         return rootView;
